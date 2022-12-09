9285552dc25e1de7e91f7b69b924085ac0b62d2e
dreamking60 <dreamlyboyczh@foxmail.com>
2022-12-09 01:07:54 -0800

    Ch-3 shadow

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let x = 5;
+    //immutable
+    //let x = 5;
+    //mutable
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The const is: {THREE_HOURS_IN_SECONDS}");
+
+    let y = 5;
+    let y = y+1;
+
+    {
+        let y = y*2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
 }
