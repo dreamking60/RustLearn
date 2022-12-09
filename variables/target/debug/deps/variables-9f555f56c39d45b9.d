edf3fc18932dafbee078a4d75227a89de0110c7d
dreamking60 <dreamlyboyczh@foxmail.com>
2022-12-09 00:36:33 -0800

    Ch3-immutable

---
+++ variables/target/debug/deps/variables-9f555f56c39d45b9.d
@@ -0,0 +1,5 @@
+/home/czh/rustLearn/variables/target/debug/deps/variables-9f555f56c39d45b9: src/main.rs
+
+/home/czh/rustLearn/variables/target/debug/deps/variables-9f555f56c39d45b9.d: src/main.rs
+
+src/main.rs:
