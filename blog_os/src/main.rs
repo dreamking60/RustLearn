e935f22bfe9ac04eeec816ffbfca6f2a4d9f20b3
dreamking60 <dreamlyboyczh@foxmail.com>
2023-01-01 20:58:44 -0800

    blog_os init

---
+++ blog_os/src/main.rs
@@ -0,0 +1,20 @@
+// in main.rs
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since 
+    // the linker looks for a function named `_start` by default
+    loop{}
+}
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+
