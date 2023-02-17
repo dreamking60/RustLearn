4b974e822bc251f79e863a1cd91698f8a17089f6
dreamking60 <dreamlyboyczh@foxmail.com>
2023-02-17 03:33:25 -0800

    Testing std::env::args

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,7 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("not path given");
+
 }
